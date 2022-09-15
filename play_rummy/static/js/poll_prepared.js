84b2d0a3bde61991a98349fef6ef037bc9485184
Evrim Öztamur <evrimoztamur@gmail.com>
2022-09-15 13:28:16 +0200

    Prevent polling continuing upon error

--- play_rummy/static/js/poll_prepared.js
+++ play_rummy/static/js/poll_prepared.js
@@ -1,10 +1,14 @@
 let ticks = document.getElementById("ticks").value | 0;;
 let gsqu = document.getElementById("state_query_uri").value;
 
-let timer = setInterval(() => {
-    fetch(gsqu)
-        .then((response) => response.json())
-        .then((data) => compareState(data));
+let timer = setInterval(async () => {
+    try {
+        await fetch(gsqu, { redirect: "error" })
+            .then((response) => response.json())
+            .then((data) => compareState(data));
+    } catch {
+        clearInterval(timer);
+    }
 }, 1000);
 
 function compareState(state) {
--- play_rummy/static/js/poll_started.js
+++ play_rummy/static/js/poll_started.js
@@ -2,10 +2,14 @@ let numActions = document.getElementById("num_actions").value | 0;;
 let state = document.getElementById("game_state").value | 0;
 let gsqu = document.getElementById("game_state_query_uri").value;
 
-let timer = setInterval(() => {
-    fetch(gsqu)
-        .then((response) => response.json())
-        .then((data) => compareGameState(data));
+let timer = setInterval(async () => {
+    try {
+        await fetch(gsqu, { redirect: "error" })
+            .then((response) => response.json())
+            .then((data) => compareGameState(data));
+    } catch {
+        clearInterval(timer);
+    }
 }, 1000);
 
 function compareGameState(gameState) {
